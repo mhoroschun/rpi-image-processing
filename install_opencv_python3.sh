69bf6dec9c0654cc5f097387a192112c4c64ee5c
Matthew Horoschun <matthew@horoschun.id.au>
2017-09-11 09:08:04 +1000

    Switch to OpenCV 3.2.0 to resolve issue with Recognizer#predict not returning a tuple. https://github.com/opencv/opencv_contrib/issues/512

--- install_opencv_python3.sh
+++ install_opencv_python3.sh
@@ -7,16 +7,17 @@ sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y libxvidcore-dev libx264-dev
 sudo apt-get install -y libgtk2.0-dev
 sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y libprotobuf-dev
 
 cd ~
 git clone https://github.com/Itseez/opencv.git
 cd opencv
-git checkout 3.1.0
+git checkout 3.2.0
 
 cd ~
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv_contrib
-git checkout 3.1.0
+git checkout 3.2.0
 
 sudo pip3 install numpy
 mkdir -p ~/opencv
@@ -30,6 +31,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D INSTALL_C_EXAMPLES=OFF \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D CPACK_BINARY_DEB:BOOL=ON \
     -D BUILD_EXAMPLES=ON .. 
 
 make -j4
