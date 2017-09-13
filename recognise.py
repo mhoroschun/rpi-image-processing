e8e937727cd93603d0a1a6b44b5cf1dd1cf4e5d5
Matthew Horoschun <matthew@horoschun.id.au>
2017-09-13 13:12:11 +1000

    Clean up the AWS recognise to make it much simpler for class

--- recognise.py
+++ recognise.py
@@ -5,18 +5,6 @@ import time
 import io
 import pprint
 import boto3
-import threading
-import signal
-import sys
-
-done = False
-
-def signal_handler(signal, frame):
-        print('You pressed Ctrl+C!')
-        done = True
-        sys.exit(0)
-        
-signal.signal(signal.SIGINT, signal_handler)
 
 camera = PiCamera()
 
@@ -25,56 +13,43 @@ W, H = 640, 480
 camera.resolution = (W, H)
 camera.framerate = 10
 
-
-class MonitorThread(threading.Thread):
-    def run (self):
-
-        camera.start_preview(fullscreen=False, window=(20,20,640,480))
+camera.start_preview(fullscreen=False, window=(20,20,640,480))
        
-        while not done:
-          time.sleep(1)
+rekognition = boto3.client('rekognition')
 
+while True:
+    
+    camera.annotate_text = "Detecting..."
 
+    frame_to_decode = io.BytesIO()
+    camera.capture(frame_to_decode, 'jpeg')
+    frame_to_decode.seek(0)
 
-class AnalysisThread(threading.Thread):
+    # Send to AWS Rekognition to detect faces.
+    response = rekognition.detect_faces(
+        Image={
+            'Bytes': frame_to_decode.read()
+        },
+        Attributes=["ALL"]
+    )
+    
+    # Print out API response to console        
+    pprint.pprint(response)
 
-    def run (self):
-        rekognition = boto3.client('rekognition')
-
-        while not done:
-            frame_to_decode = io.BytesIO()
-
-            camera.capture(frame_to_decode, 'jpeg')
-            frame_to_decode.seek(0)
-            camera.annotate_text = "Detecting..."
-            response = rekognition.detect_faces(
-                Image={
-                    'Bytes': frame_to_decode.read()
-                },
-                Attributes=["ALL"]
+    # Select out some attributes and display them on the screen
+    if response and response['FaceDetails'] and response['FaceDetails'][0]:
+        camera.annotate_text = (
+            str(response['FaceDetails'][0]['AgeRange']['Low']) + " - " +
+            str(response['FaceDetails'][0]['AgeRange']['High']) + " year old " +
+            response['FaceDetails'][0]['Gender']['Value']
+        )
+        if response['FaceDetails'][0]['Emotions'][0]:
+            camera.annotate_text += (
+                "(" + response['FaceDetails'][0]['Emotions'][0]['Type'] + ")"
             )
-            
-            pprint.pprint(response)
+    else:
+        camera.annotate_text = "Is anyone there?"
 
-            if response and response['FaceDetails'] and response['FaceDetails'][0]:
-              camera.annotate_text = (
-                str(response['FaceDetails'][0]['AgeRange']['Low']) + " - " +
-                str(response['FaceDetails'][0]['AgeRange']['High']) + " year old " +
-                response['FaceDetails'][0]['Gender']['Value']
-              )
-              if response['FaceDetails'][0]['Emotions'][0]:
-                camera.annotate_text += (
-                  "(" + response['FaceDetails'][0]['Emotions'][0]['Type'] + ")"
-                )
-            else:
-              camera.annotate_text = "Is anyone there?"
- 
-            time.sleep(1)
+    time.sleep(1)
 
-
-monitor = MonitorThread()
-analyzer = AnalysisThread()
-monitor.start()
-analyzer.start()
-monitor.join()
 
